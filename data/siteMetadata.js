f99d0126513544eadec6194095c2b680e6a48a67
nagacoder <nagacoder@outlook.com>
2025-03-31 20:46:07 +0700

    fix: remove default url

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,8 +6,8 @@ const siteMetadata = {
   description: 'Personal blog by me. I explain with words and code.',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://notes.darvinsinaga.com',
+  siteRepo: 'https://github.com/nagacoder/simple-personal-tech-blog',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
   email: 'nagacoder@outlook.com',
