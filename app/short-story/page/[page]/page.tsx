a2466f7f72268c86e5a6c4500d003ee1372937f5
nagacoder <nagacoder@outlook.com>
2025-07-22 20:18:09 +0700

    feat: add short story pages

---
+++ app/short-story/page/[page]/page.tsx
@@ -0,0 +1,43 @@
+import ListLayout from '@/layouts/ListLayout'
+import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
+import { allShortStories } from 'contentlayer/generated'
+import { notFound } from 'next/navigation'
+
+const POSTS_PER_PAGE = 5
+
+export const generateStaticParams = async () => {
+  const totalPages = Math.ceil(allShortStories.length / POSTS_PER_PAGE)
+  const paths = Array.from({ length: totalPages }, (_, i) => ({ page: (i + 1).toString() }))
+
+  return paths
+}
+
+export default async function Page(props: { params: Promise<{ page: string }> }) {
+  const params = await props.params
+  const posts = allCoreContent(sortPosts(allShortStories))
+  const pageNumber = parseInt(params.page as string)
+  const totalPages = Math.ceil(posts.length / POSTS_PER_PAGE)
+
+  // Return 404 for invalid page numbers or empty pages
+  if (pageNumber <= 0 || pageNumber > totalPages || isNaN(pageNumber)) {
+    return notFound()
+  }
+  const initialDisplayPosts = posts.slice(
+    POSTS_PER_PAGE * (pageNumber - 1),
+    POSTS_PER_PAGE * pageNumber
+  )
+  const pagination = {
+    currentPage: pageNumber,
+    totalPages: totalPages,
+  }
+
+  return (
+    <ListLayout
+      isSearchAble
+      posts={posts}
+      initialDisplayPosts={initialDisplayPosts}
+      pagination={pagination}
+      title="All Posts"
+    />
+  )
+}
--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,6 +2,7 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
+  { href: '/short-story', title: 'Short Story' },
   { href: '/archives', title: 'Archives' },
   { href: '/about', title: 'About' },
 ]
